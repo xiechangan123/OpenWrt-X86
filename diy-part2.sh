3770894f246b0ababdfdc6ab10e5c5fdecde4256
xiechangan123 <xiechangan123@139.com>
2025-10-21 11:20:45 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,10 +18,3 @@
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
-
-# Modify default Password
-sed -i 's/root:::0:99999:7:::/root:$1$0MHbqDVw$FMCswXfbF5apRBrTWoVfA0:19333:0:99999:7:::/g' package/base-files/files/etc/shadow
-
-# Modify default TimeZone
-sed -i 's/UTC/CST-8/g' package/base-files/files/bin/config_generate
-sed -i "/.*ttylogin='0'.*/i\		set system.@system[-1].zonename='Asia/Shanghai'" package/base-files/files/bin/config_generate
