18af39afec139e93a8921d972521abd659fb6140
xiechangan123 <xiechangan123@139.com>
2026-01-22 00:28:34 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -27,7 +27,7 @@ sed -i 's/UTC/CST-8/g' package/base-files/files/bin/config_generate
 sed -i "/.*ttylogin='0'.*/i\		set system.@system[-1].zonename='Asia/Shanghai'" package/base-files/files/bin/config_generate
 
 # update sing-box
-#rm -rf ./feeds/packages/net/sing-box
-#git clone https://github.com/immortalwrt/packages.git temp-packages
-#cp -rf ./temp-packages/net/sing-box ./feeds/packages/net/sing-box
-#rm -rf temp-packages
+rm -rf ./feeds/packages/net/sing-box
+git clone https://github.com/immortalwrt/packages.git temp-packages
+cp -rf ./temp-packages/net/sing-box ./feeds/packages/net/sing-box
+rm -rf temp-packages
