174d6bf9a5b31d93894a1dd99105d22eb3366f85
xiechangan123 <xiechangan123@139.com>
2026-02-02 23:21:13 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,9 +18,8 @@
 #echo 'src-git passwall https://github.com/Openwrt-Passwall/openwrt-passwall' >>feeds.conf.default
 
 # Use github mirror 
-sed -i 's|git.openwrt.org/openwrt|github.com/openwrt|g' feeds.conf.default
 sed -i 's|git.openwrt.org/feed|github.com/openwrt|g' feeds.conf.default
-sed -i 's|git.openwrt.org|github.com/openwrt|g' feeds.conf.default
+sed -i 's|git.openwrt.org/project|github.com/openwrt|g' feeds.conf.default
 
 # Add packages
 git clone https://github.com/xiechangan123/homeproxy package/homeproxy
