64eb1731a03db3d267c79e537938bac9e1d5c0db
xiechangan123 <xiechangan123@139.com>
2026-02-02 17:15:21 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,7 +16,12 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/Openwrt-Passwall/openwrt-passwall' >>feeds.conf.default
-sed -i 's|git.openwrt.org|github.com/openwrt|g' feeds.conf.default
+
+# Use github mirror 
+src-git packages https://github.com/openwrt/packages.git
+src-git luci https://github.com/openwrt/luci.git
+src-git routing https://github.com/openwrt-routing/packages.git
+src-git telephony https://github.com/openwrt/telephony.git
 
 # Add packages
 git clone https://github.com/xiechangan123/homeproxy package/homeproxy
