cf0d9a2b0fd8bc289dd3e8758a92cf7eb39e2497
xiechangan123 <xiechangan123@139.com>
2026-03-24 01:01:17 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -23,3 +23,9 @@ sed -i 's|git.openwrt.org/project|github.com/openwrt|g' feeds.conf.default
 
 # Add packages
 git clone https://github.com/xiechangan123/homeproxy package/homeproxy
+
+mkdir -p files/usr/bin
+LATEST_TAG=$(wget -qO- "https://api.github.com/repos/SagerNet/sing-box/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+wget -qO- "https://github.com/SagerNet/sing-box/releases/download/${LATEST_TAG}/sing-box_${LATEST_TAG#v}_openwrt_x86_64.apk" | \
+  unzip -p - usr/bin/sing-box > files/usr/bin/sing-box
+chmod +x files/usr/bin/sing-box
