9305bf10b5200510c2e3618cce76b0b9ccc8dca1
xiechangan123 <xiechangan123@139.com>
2026-03-24 20:03:05 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -23,8 +23,3 @@ sed -i 's|git.openwrt.org/project|github.com/openwrt|g' feeds.conf.default
 
 # Add packages
 git clone https://github.com/xiechangan123/homeproxy package/homeproxy
-
-# use singbox1.13.3
-mkdir -p files/usr/bin
-wget -qO- "https://github.com/SagerNet/sing-box/releases/download/v1.13.3/sing-box-1.13.3-linux-amd64-musl.tar.gz" | tar -xz -C files/usr/bin --strip-components=1 --wildcards '*/sing-box'
-chmod +x files/usr/bin/sing-box
