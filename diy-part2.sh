2190d3484d18b4c6c8b77a27824c706a99af7de0
xiechangan123 <xiechangan123@139.com>
2026-08-12 22:54:56 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -22,5 +22,5 @@
 # Use sing-box-1.13.18
 #echo "🔍 正在获取 sing-box 1.13.18 的正确 HASH..."
 #wget -qO- https://codeload.github.com/SagerNet/sing-box/tar.gz/v1.13.18 | sha256sum | awk '{print $1}'
-sed -i 's/PKG_VERSION:=.*/PKG_VERSION:=1.13.18/g' feeds/packages/net/sing-box/Makefile
-sed -i 's/PKG_HASH:=.*/PKG_HASH:=e41ed9d7adecd7597c1d5cc91818366a9538d94b41c244225ac40ac948c643f5/g' feeds/packages/net/sing-box/Makefile
+#sed -i 's/PKG_VERSION:=.*/PKG_VERSION:=1.13.18/g' feeds/packages/net/sing-box/Makefile
+#sed -i 's/PKG_HASH:=.*/PKG_HASH:=e41ed9d7adecd7597c1d5cc91818366a9538d94b41c244225ac40ac948c643f5/g' feeds/packages/net/sing-box/Makefile
