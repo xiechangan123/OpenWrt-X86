e7d358d01d17708dfc4c81d352aa4cf7366dc813
xiechangan123 <xiechangan123@139.com>
2026-08-30 08:38:41 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,8 +19,8 @@
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
 
-# Use sing-box-1.13.19
-#echo "🔍 正在获取 sing-box 1.13.19 的正确 HASH..."
-#wget -qO- https://codeload.github.com/SagerNet/sing-box/tar.gz/v1.13.19 | sha256sum | awk '{print $1}'
-sed -i 's/PKG_VERSION:=.*/PKG_VERSION:=1.13.19/g' feeds/packages/net/sing-box/Makefile
-sed -i 's/PKG_HASH:=.*/PKG_HASH:=abc2f4805b3fd088c18a5694b51fed6f0e1d06632fae98029d6bf7bd79a1b3a2/g' feeds/packages/net/sing-box/Makefile
+# Use sing-box-1.13.20
+#echo "🔍 正在获取 sing-box 1.13.20 的正确 HASH..."
+#wget -qO- https://codeload.github.com/SagerNet/sing-box/tar.gz/v1.13.20 | sha256sum | awk '{print $1}'
+sed -i 's/PKG_VERSION:=.*/PKG_VERSION:=1.13.20/g' feeds/packages/net/sing-box/Makefile
+sed -i 's/PKG_HASH:=.*/PKG_HASH:=49565451a11279b6213084c56cdc80f796da9c96571b5bf2f27c86af0ad74e85/g' feeds/packages/net/sing-box/Makefile
